d4bf862a00696ebdddfbb19d7231687ffe86c4ea
tfer2442 <tfer2442@gmail.com>
2023-09-07 23:46:07 +0900

    feature/ch06.sec07.exam05

--- src/ch04/sec07/BreakOutterExample.java
+++ src/ch04/sec07/BreakOutterExample.java
@@ -2,6 +2,9 @@ package ch04.sec07;
 
 public class BreakOutterExample {
     public static void main(String[] args) {
+        int a = 5;
+        char b = 'a';
+        System.out.println(Integer.parseInt("12345"));
         Outter: for(char upper='A'; upper<='Z'; upper++){
         for(char lower='a'; lower<='z'; lower++){
             System.out.println(upper+"-"+lower);
---
+++ src/ch06/sec07/exam05/CarExample.java
@@ -0,0 +1,11 @@
+package ch06.sec07.exam05;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car car1 = new Car("genesis");
+
+        System.out.println(car1.color);
+        System.out.println(car1.company);
+
+    }
+}
